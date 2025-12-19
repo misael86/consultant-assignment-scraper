ffb847b60f7f93979e7f0e06c2a65c8753562186
Misael Berrios Salas <misael.b.salas@gmail.com>
2025-12-19 21:52:16 +0100

    remove all assignments older than 1 week

--- src/app/api/assignments/route.ts
+++ src/app/api/assignments/route.ts
@@ -5,5 +5,12 @@ import { IAssignment } from "@/lib/scrape-response";
 export async function GET() {
   console.log("GET assignments");
   const database = await JSONFilePreset<IAssignment[]>("./public/assignments.json", []);
+  database.data = database.data.filter((assignment) => {
+    const lastWeek = new Date();
+    const lastWeekDate = lastWeek.getUTCDate() - 7;
+    lastWeek.setUTCDate(lastWeekDate);
+    return assignment.scraped > lastWeek.toLocaleDateString("sv-SE");
+  });
+  await database.write();
   return Response.json(database.data);
 }
