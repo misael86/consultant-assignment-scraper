12363f940fa7bc8f32be0a2add73e33f7e980c9e
Misael Berrios Salas <misael.b.salas@gmail.com>
2025-12-19 22:08:36 +0100

    save latest from each source to prevent rescrape of everything

--- src/app/api/assignments/route.ts
+++ src/app/api/assignments/route.ts
@@ -1,3 +1,4 @@
+import { Low } from "lowdb";
 import { JSONFilePreset } from "lowdb/node";
 
 import { IAssignment } from "@/lib/scrape-response";
@@ -5,12 +6,23 @@ import { IAssignment } from "@/lib/scrape-response";
 export async function GET() {
   console.log("GET assignments");
   const database = await JSONFilePreset<IAssignment[]>("./public/assignments.json", []);
-  database.data = database.data.filter((assignment) => {
+  await removeOldAssignments(database);
+  return Response.json(database.data);
+}
+
+async function removeOldAssignments(database: Low<IAssignment[]>) {
+  const savedSources: string[] = [];
+  const filteredData = database.data.toReversed().filter((assignment) => {
+    if (!savedSources.includes(assignment.source)) {
+      savedSources.push(assignment.source);
+      return true;
+    }
+
     const lastWeek = new Date();
     const lastWeekDate = lastWeek.getUTCDate() - 7;
     lastWeek.setUTCDate(lastWeekDate);
     return assignment.scraped > lastWeek.toLocaleDateString("sv-SE");
   });
+  database.data = filteredData.toReversed();
   await database.write();
-  return Response.json(database.data);
 }
